e76e19684c45f70c0439e2c6095433f059a0966c
Daniel Schweinfurth <46092234+DanielSchweinfurth@users.noreply.github.com>
2021-11-30 17:01:06 +0100

    Add files via upload

--- LineareListe.java
+++ LineareListe.java
@@ -23,29 +23,56 @@ public class LineareListe {
 		this.start = start;
 	}
 	
-	public int getLaenge(Element e) {
-		if(e.getPointer() != null) {
-			laenge ++;
-			getLaenge(e.getPointer());
-		}
+	public int getLength(){
 		return laenge;
 	}
+//	
+//	public int getLaenge() {
+//		if(this.start == null) {
+//			return 0;
+//		}else {
+//		return getLaenge(this.start);
+//		}}
+//	
+//	public int getLaenge(Element e) {
+//		if(e.getPointer() != null) {
+//			laenge ++;
+//			getLaenge(e.getPointer());
+//		}
+//		return laenge;
+//	}
+//	
 	
-	
-	public void setlaenge(int laenge) {
-		this.laenge = laenge;
-	}
 	
 	//Methoden
-	public Element getLastRec() {
-		return getlastRe(start);
+	public void Add(Element b) {
+		if(this.start == null) {
+			this.start = b;
+			laenge ++;
+		}else {
+			this.getLastIterativ().setPointer(b);
+			laenge ++;
+		}
+		
+		
 	}
 	
-	public Element getlastRe(Element k) {
-		if(k != null) {
-			getlastRe(k.getPointer());
-			return k;
-		}else {return null;}
+	
+	public Element getLastRekursiv() {
+		return getlastRe(start);
+	}
+	private Element getlastRe(Element k) {
+		if(k.getPointer() != null) {
+			k = getlastRe(k.getPointer());
+		}
+		return k;
+	}
+	
+	public Element getLastIterativ() { 
+		Element e = start;
+		while(e.getPointer() != null) {
+			e = e.getPointer();
+		}return e;
 	}
 	
 	public boolean istLeer() {
@@ -57,16 +84,33 @@ public class LineareListe {
 		
 	}
 
-	
+/*
 	public boolean greaterLength(LineareListe comp) {
-		if(comp.getLaenge(comp.start) >= //was für ein statmenet brauche ich hier um liste.greaterLength(zu vergleichende Liste zu machen?)) {
+		if(comp.getLaenge(comp.start) >= //was für ein statmenet brauche ich hier um liste.greaterLength(zu vergleichende Liste zu machen?)) 
+				{
 			return true;
 		}else {
 			return false;
 		}
 		
 	}
-
+*/
 	
+	public boolean greaterLength(LineareListe compare) {
+		if(compare.getLength() <= this.getLength() ) {
+			return true;
+		}else {
+			return false;
+		}
+				
+	}
+	
+	public void appendList(LineareListe a) {
+		if(a.getLength() <= this.getLength()) {
+			this.getLastRekursiv().setPointer(a.start);
+		}else {
+			a.getLastIterativ().setPointer(this.start);
+		}
+	}
 }
 
--- Test.java
+++ Test.java
@@ -6,21 +6,37 @@ public class Test {
 		LineareListe liste1 = new LineareListe();
 		
 		Element b = new Element("zwei");
+		liste.Add(b);
 		Element c = new Element("drei");
+		liste.Add(c);
 		Element d = new Element("vier");
+		liste.Add(d);
 		Element e = new Element("fünf");
+		liste.Add(e);
 		Element f = new Element("sechs");
+		liste.Add(f);
 		
-		a.setPointer(b);
-		b.setPointer(c);
-		c.setPointer(d);
-		d.setPointer(e);
-		e.setPointer(f);
+		Element aa = new Element("1");
+		liste1.Add(aa);
+		Element ab = new Element("2");
+		liste1.Add(ab);
+		Element ac = new Element("3");
+		liste1.Add(ac);
+		Element ad = new Element("4");
+		liste1.Add(ad);
+		Element ae = new Element("5");
+		liste1.Add(ae);
 		
-		System.out.println(liste.getLast().getInhalt());
-
-		System.out.println(liste.getLaenge());
-		System.out.println(liste1.getLaenge());
-		System.out.print(liste1.greaterLength(liste));
+//		System.out.println(liste.getLastIterativ().getInhalt());
+//		System.out.println(liste.getLastRekursiv().getInhalt());
+//		
+//		System.out.println(liste.getLength());
+//		System.out.println(liste1.getLength());
+//		System.out.print(liste1.greaterLength(liste));
+		
+		liste.appendList(liste1);
+		
+		
+		
 	}
 }
