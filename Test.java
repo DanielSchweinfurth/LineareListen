9813a9d9e1592d63d7c2e2256f0ae4c5e5551bac
Daniel Schweinfurth <46092234+DanielSchweinfurth@users.noreply.github.com>
2021-11-30 13:46:50 +0100

    Add files via upload

--- Test.java
+++ Test.java
@@ -13,22 +13,27 @@ public class Test {
 		
 		a.setPointer(b);
 		b.setPointer(c);
-		//c.setPointer(d);
+		c.setPointer(d);
 		d.setPointer(e);
 		e.setPointer(f);
 		
-//		System.out.println("Liste Full");
+		System.out.println("Liste Full");
 		System.out.println(liste.getLaenge(liste.getStart()));
-//		System.out.println(liste.istLeer());
-//		System.out.println(liste.getLastRec());
-//		
-//		System.out.println("");
-//		
-//		System.out.println("Liste1 empty");
-//		System.out.println(liste1.getLaenge(liste.getStart()));
-//		System.out.println(liste1.istLeer());
-//		System.out.println(liste1.getLastRec());
+		System.out.println(liste.istLeer());
+		System.out.println(liste.getlastRe(liste.getStart()).getInhalt());// sollte sechs ausgeben es wird aber eins ausgegeben ?
+		System.out.println(liste.greaterLength(liste,liste1));
+		
+		System.out.println("");
+		
+		System.out.println("Liste1 empty");
+		System.out.println(liste1.getLaenge(liste.getStart()));
+		System.out.println(liste1.istLeer());
+		System.out.println(liste1.getLastRec());
 
-		System.out.println(liste1.getLaenge(liste1.getStart()));
+	}
+
+	private static char[] greaterLength() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
