bcafbe3bce1144c201ccc20f7aef883325b08b56
robW-dev <67891344+robW-dev@users.noreply.github.com>
2021-11-30 18:01:03 +0100

    Add files via upload

--- LineareListe.java
+++ LineareListe.java
@@ -1,116 +1,150 @@
 import java.util.*;
+
 public class LineareListe {
-	//Atribute
+	// Atribute
 	private Element start;
 	private int laenge;
-	
-	//Konstruktor
+
+	// Konstruktor
 	public LineareListe() {
 		start = null;
 		laenge = 0;
 	}
+
 	public LineareListe(Element start) {
 		this.start = start;
 		laenge = 1;
 	}
-	
-	//Getter u. Setter
+
+	// Getter u. Setter
 	public Element getStart() {
 		return start;
 	}
-	
+
 	public void setStart(Element start) {
 		this.start = start;
 	}
-	
-	public int getLength(){
+
+	public int getLength() {
 		return laenge;
 	}
-//	
-//	public int getLaenge() {
-//		if(this.start == null) {
-//			return 0;
-//		}else {
-//		return getLaenge(this.start);
-//		}}
-//	
-//	public int getLaenge(Element e) {
-//		if(e.getPointer() != null) {
-//			laenge ++;
-//			getLaenge(e.getPointer());
-//		}
-//		return laenge;
-//	}
-//	
-	
-	
-	//Methoden
-	public void Add(Element b) {
-		if(this.start == null) {
-			this.start = b;
-			laenge ++;
-		}else {
-			this.getLastIterativ().setPointer(b);
-			laenge ++;
+
+	public int getLaenge() {
+		if (this.start == null) {
+			return 0;
+		} else {
+			return getLaenge2();
 		}
-		
-		
 	}
+
+	private int getLaenge2() {
+		Element currentElement = this.start;
+		while(currentElement.getPointer() != null)
+		{
+			this.laenge++;
+			currentElement = currentElement.getPointer();
+		}		
+		return laenge;
+	}
+
 	
-	
-	public Element getLastRekursiv() {
+
+	// Methoden
+	public Element getLast() {
 		return getlastRe(start);
 	}
+
 	private Element getlastRe(Element k) {
-		if(k.getPointer() != null) {
-			k = getlastRe(k.getPointer());
+
+		Element currentElement = k;
+
+		if (currentElement.getPointer() != null) {
+			currentElement = getlastRe(currentElement.getPointer());
+
 		}
-		return k;
+
+		return currentElement;
 	}
 	
-	public Element getLastIterativ() { 
+	public void appendList(LineareListe listeToAppend)
+	{
+		if(this.getLaenge() >= listeToAppend.getLaenge())
+		{
+			this.getLast().setPointer(listeToAppend.getStart());
+		}
+		else
+		{
+			listeToAppend.getLast().setPointer(this.getStart());
+		}
+	}	
+
+	public Element getLastIterativ() { // Das hier geht nicht was mache ich
+										// falsch
 		Element e = start;
-		while(e.getPointer() != null) {
+		while (e.getPointer() != null) {
 			e = e.getPointer();
-		}return e;
+		}
+		return e;
 	}
-	
+
 	public boolean istLeer() {
-		if(start == null) {
+		if (start == null) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
-		
+
 	}
 
-/*
+	/*
+	 * public boolean greaterLength(LineareListe comp) {
+	 * if(comp.getLaenge(comp.start) >= //was für ein statmenet brauche ich
+	 * hier um liste.greaterLength(zu vergleichende Liste zu machen?)) { return
+	 * true; }else { return false; }
+	 * 
+	 * }
+	 */
+
 	public boolean greaterLength(LineareListe comp) {
-		if(comp.getLaenge(comp.start) >= //was für ein statmenet brauche ich hier um liste.greaterLength(zu vergleichende Liste zu machen?)) 
-				{
+		if (comp.getLaenge() <= this.getLaenge()) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
+
+	}
+
+	public void Add(Element b) {
+		if (this.start == null) {
+			this.start = b;
+		} else {
+			this.getLast().setPointer(b);
+		}
+	}
+	
+	public boolean isSorted()
+	{
+		Element currentElement = this.start;
+		Element nextElement = null;
+		
+		while(currentElement.getPointer() != null)
+		{
+			nextElement = currentElement.getPointer();
+					
+			while(nextElement.getPointer() != null)
+			{
+				if(currentElement.getInhalt().compareTo(nextElement.getInhalt()) > 0)
+				{
+					return false;
+				}
+				nextElement = nextElement.getPointer();
+			}
+			currentElement = currentElement.getPointer();				
+		}
+		return true;
 		
 	}
-*/
 	
-	public boolean greaterLength(LineareListe compare) {
-		if(compare.getLength() <= this.getLength() ) {
-			return true;
-		}else {
-			return false;
-		}
-				
-	}
 	
-	public void appendList(LineareListe a) {
-		if(a.getLength() <= this.getLength()) {
-			this.getLastRekursiv().setPointer(a.start);
-		}else {
-			a.getLastIterativ().setPointer(this.start);
-		}
-	}
-}
 
+}
